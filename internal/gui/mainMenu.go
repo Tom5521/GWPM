4ae4ae95597d3348a438559abf68999995759e61
Tom5521 <thomas.bored5521@gmail.com>
2024-03-31 17:30:09 -0400

    feat(internal/gui): Finish the issue and comment on the manager element and leave it for later.

--- internal/gui/mainMenu.go
+++ internal/gui/mainMenu.go
@@ -2,6 +2,7 @@ package gui
 
 import (
 	"fyne.io/fyne/v2"
+	"github.com/Tom5521/GWPM/pkg/gui/popups"
 )
 
 type MainMenu struct {
@@ -10,14 +11,15 @@ type MainMenu struct {
 
 func (m *MainMenu) Init() {
 	m.Menu = fyne.NewMainMenu(
-		fyne.NewMenu("Manager",
-			fyne.NewMenuItem("Reload", func() {
-				cui.InitPkgSlice()
-			}),
-			fyne.NewMenuItem("Change", func() {
-				// TODO: Finish this.
-			}),
-		),
+		/*
+			fyne.NewMenu("Manager",
+				fyne.NewMenuItem("Reload", func() {
+					cui.InitPkgSlice()
+				}),
+				fyne.NewMenuItem("Change", func() {
+
+				}),
+			),*/
 		fyne.NewMenu("Packages",
 			fyne.NewMenuItem("Select All", func() {
 				for i := range cui.list.Length() {
@@ -39,8 +41,30 @@ func (m *MainMenu) Init() {
 					cui.list.RefreshItem(i)
 				}
 			}),
-			fyne.NewMenuItem("Install selected", func() {}),
-			fyne.NewMenuItem("Uninstall selected", func() {}),
+			fyne.NewMenuItem("Install selected", func() {
+				for i := range cui.list.Length() {
+					p := cui.packages[i]
+					if !p.Checked {
+						continue
+					}
+					err := p.Install()
+					if err != nil {
+						popups.Error(err)
+					}
+				}
+			}),
+			fyne.NewMenuItem("Uninstall selected", func() {
+				for i := range cui.list.Length() {
+					p := cui.packages[i]
+					if !p.Checked {
+						continue
+					}
+					err := p.Uninstall()
+					if err != nil {
+						popups.Error(err)
+					}
+				}
+			}),
 		),
 	)
 }
