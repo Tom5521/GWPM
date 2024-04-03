f87c07ddccaf617da331b435b94ef6fef8f252ce
Tom5521 <thomas.bored5521@gmail.com>
2024-04-03 19:12:17 -0400

    fix(pkg/choco): Fix not administrator permissions in Manager.InstallManager

--- pkg/choco/manager.go
+++ pkg/choco/manager.go
@@ -244,6 +244,9 @@ func (m *Manager) InstallManager() error {
 	if m.isInstalled {
 		return pkg.ErrManagerIsInstalled
 	}
+	if !perm.IsAdmin {
+		return pkg.ErrNotAdministrator
+	}
 	const command = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 	cmd := exec.Command("powershell", "-c", command)
 	cmd.Stdin = os.Stdin
